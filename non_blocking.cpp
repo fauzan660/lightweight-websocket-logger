41f936e6a56ec7536bceb4eaf4d345fb908ce677
fauzan <fauzantahir660@gmail.com>
2026-05-26 15:34:52 +0500

    completed websocket handshake

--- non_blocking.cpp
+++ non_blocking.cpp
@@ -16,9 +16,11 @@
 #include <unistd.h>
 using namespace std;
 #define MAX_CONNECTIONS 5
-#define MAX_CLIENTS 10
+#define MAX_CLIENTS 20
 #define BUF_SIZE 4096
 #define MAGIC_WEBSOCKET_UUID_STRING "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+#define WS_ENDPOINT "/websocket"
+#define MAX_WS_CLIENTS 10
 
 typedef struct {
   int fd;
@@ -39,37 +41,6 @@ string encode_key_base64(string &key) {
   EVP_EncodeBlock((unsigned char *)output.data(), (unsigned char *)key.c_str(),
                   key.size());
   return output;
-}
-
-bool check_request_validity(string method, string target, string http_version,
-                            map<string, string> headers_map) {
-
-  if (method == "GET") {
-    string http_version_number =
-        http_version.substr(http_version.find('/') + 1, http_version.length());
-    int http_vn = stoi(http_version_number);
-    if (http_vn >= 1.1) {
-      auto upg = headers_map.find("upgrade");
-      if (upg != headers_map.end() && upg->second == "websocket") {
-        auto con = headers_map.find("connection");
-        if (con != headers_map.end() && con->second == "Upgrade") {
-          auto key = headers_map.find("sec-websocket-key");
-          if (key != headers_map.end()) {
-            return true;
-          }
-        }
-      }
-    }
-  }
-  return false;
-}
-
-string generate_websocket_response_key(string websocket_request_key) {
-  string magic_string_concat =
-      websocket_request_key + MAGIC_WEBSOCKET_UUID_STRING;
-  string hashed_string = encode_key_openssl(magic_string_concat);
-  string encoded_string = encode_key_base64(hashed_string);
-  return encoded_string;
 }
 
 void parse_request(string message, string &method, string &target,
@@ -94,6 +65,16 @@ void parse_request(string message, string &method, string &target,
       headers_map[key] = val;
     }
     headers_str = headers_str.substr(pos + 2);
+  }
+  if (!headers_str.empty()) {
+    size_t colon = headers_str.find(": ");
+    if (colon != string::npos) {
+      string key = headers_str.substr(0, colon);
+      string val = headers_str.substr(colon + 2);
+      for (auto &ch : key)
+        ch = tolower(ch);
+      headers_map[key] = val;
+    }
   }
 }
 
@@ -123,6 +104,79 @@ int setup_server_socket(int &s, struct sockaddr_in &address, int PORT) {
   return 1;
 }
 
+string generate_websocket_response_key(string websocket_request_key) {
+  string magic_string_concat =
+      websocket_request_key + MAGIC_WEBSOCKET_UUID_STRING;
+  string hashed_string = encode_key_openssl(magic_string_concat);
+  string encoded_string = encode_key_base64(hashed_string);
+  return encoded_string;
+}
+bool check_request_ws(string method, string target, string http_version,
+                      map<string, string> headers_map) {
+
+  if (method != "GET") {
+    printf("check ws request: method is not GET \n");
+    return false;
+  }
+  string http_version_number =
+      http_version.substr(http_version.find('/') + 1, http_version.length());
+  float http_vn = stof(http_version_number);
+  if (http_vn < 1.1) {
+
+    printf("check ws request: http version is deprecated \n");
+    return false;
+  }
+  auto upg = headers_map.find("upgrade");
+  if (upg == headers_map.end() && upg->second != "websocket") {
+    printf("check ws request: upgrade header was missing or incorrect \n");
+    return false;
+  }
+  auto con = headers_map.find("connection");
+  if (con == headers_map.end() && con->second != "Upgrade") {
+    printf("check ws request: connection header was missing or incorrect \n");
+    return false;
+  }
+  auto key = headers_map.find("sec-websocket-key");
+  if (key == headers_map.end()) {
+    printf("check ws request: ws key not present in headers \n");
+    return false;
+  }
+  return true;
+}
+int handle_request_ws(Client *c, map<string, string> headers_map,
+                      Client websocket_clients[], Client clients[]) {
+  for (int i = 0; i < MAX_WS_CLIENTS; i++) {
+    if (websocket_clients[i].fd == -1) {
+      websocket_clients[i].fd = c->fd;
+      for (int j = 0; j < MAX_CLIENTS; j++) {
+        if (clients[j].fd == c->fd) {
+          clients[j].fd = -1;
+          break;
+        }
+      }
+      break;
+    }
+  }
+
+  string ws_response = "HTTP/1.1 101 Switching Protocols\r\n"
+                       "Upgrade: websocket\r\n"
+                       "Connection: Upgrade\r\n";
+  auto key = headers_map.find("sec-websocket-key");
+  if (key != headers_map.end()) {
+    string ws_response_key = generate_websocket_response_key(key->second);
+    ws_response += "Sec-WebSocket-Accept: " + ws_response_key +
+                   "\r\n"
+                   "\r\n";
+    printf("server generate ws response: \n%s \n", ws_response.c_str());
+    int send_status =
+        ::send(c->fd, ws_response.c_str(), strlen(ws_response.c_str()), 0);
+    return send_status;
+  } else {
+    perror("ws key not found in headers \n");
+  }
+  return -1;
+}
+
 void accept_clients(int s, Client clients[], fd_set &read_sockets,
                     struct sockaddr_in &address, int &address_len) {
   cout << "-------Accepting client connections------" << endl;
@@ -147,20 +201,24 @@ void accept_clients(int s, Client clients[], fd_set &read_sockets,
 }
 
 void handle_client(Client *c, char server_buf[], char response_buf[],
-                   fd_set &read_sockets) {
+                   char error_buf[], fd_set &read_sockets,
+                   Client websocket_clients[], Client clients[]) {
+
+  string method, target, http_version;
+  map<string, string> headers_map;
   cout << "-------Handling client connections-------" << endl;
   string message = "";
   while (true) {
     int recv_status = ::recv(c->fd, server_buf, BUF_SIZE - 1, 0);
     if (recv_status == 0) {
-      printf("server waiting timeout\n");
+      perror("recv failed -- timeout --");
       close_socket(c);
       break;
     }
     if (recv_status < 0) {
       if (errno == EAGAIN || errno == EWOULDBLOCK)
         break;
-      printf("client closed connection\n");
+      perror("recv failed -- connection closed --");
       close_socket(c);
       break;
     }
@@ -172,16 +230,38 @@ void handle_client(Client *c, char server_buf[], char response_buf[],
   if (c->fd == -1 || message.empty())
     return;
   cout << "Received message:\n" << message << endl;
-  string method, target, http_version;
-  map<string, string> headers_map;
   parse_request(message, method, target, http_version, headers_map);
   cout << "method, target, http_version: " << method << " " << target << " "
        << http_version << endl;
-  cout << "headers:" << endl;
-  for (auto &h : headers_map)
-    cout << h.first << ": " << h.second << endl;
-  ::send(c->fd, response_buf, strlen(response_buf), 0);
-  close_socket(c);
+
+  cout << "target --> " << target << endl;
+
+  int client_fd = c->fd;
+  if (target == "/websocket") {
+    printf("request to ws endpoint \n");
+    if (check_request_ws(method, target, http_version, headers_map)) {
+      int handle_request_ws_status =
+          handle_request_ws(c, headers_map, websocket_clients, clients);
+      if (handle_request_ws_status) {
+        printf("client %d request handled by server successfully \n",
+               client_fd);
+      } else {
+        perror("request handling failed");
+      }
+    } else {
+      printf("not a valid ws request \n");
+      ::send(c->fd, error_buf, strlen(error_buf), 0);
+      close(c->fd);
+    }
+  } else {
+    printf("request to http endpoint \n");
+    ::send(c->fd, response_buf, strlen(response_buf), 0);
+  }
+}
+
+void handle_websocket_client(Client *ws_c, Client clients[],
+                             Client websocket_clients[]) {
+  printf("handling websocket client with fd %d \n", ws_c->fd);
 }
 
 int main() {
@@ -191,6 +271,7 @@ int main() {
   fd_set write_sockets, read_sockets;
   struct timeval waitd;
   Client clients[MAX_CLIENTS];
+  Client websocket_clients[MAX_WS_CLIENTS];
   char server_buf[BUF_SIZE];
   char response_buf[] = "HTTP/1.1 200 OK\r\n"
                         "Content-Type: text/html; charset=utf-8\r\n"
@@ -202,6 +283,7 @@ int main() {
                         "<H1>200 OK</H1>\r\n"
                         "Welcome to the default.\r\n"
                         "</BODY></HTML>\r\n";
+  char ws_handshake_error_buf[] = "HTTP/1.1 400 Bad Request";
 
   if (setup_server_socket(s, address, PORT))
     printf("Server socket successfully setup\n");
@@ -216,6 +298,8 @@ int main() {
 
   for (int i = 0; i < MAX_CLIENTS; i++)
     clients[i].fd = -1;
+  for (int i = 0; i < MAX_WS_CLIENTS; i++)
+    websocket_clients[i].fd = -1;
 
   while (1) {
     max_fd = s;
@@ -233,6 +317,14 @@ int main() {
     if (sel < 0)
       continue;
 
+    for (int i = 0; i < MAX_WS_CLIENTS; i++) {
+      Client *ws_c = &websocket_clients[i];
+      if (ws_c->fd != -1) {
+        handle_websocket_client(ws_c, clients, websocket_clients);
+        ws_c->fd = -1;
+      }
+    }
+
     if (FD_ISSET(s, &read_sockets))
       accept_clients(s, clients, read_sockets, address, address_len);
 
@@ -241,7 +333,8 @@ int main() {
       if (c->fd == -1 || c->fd == s)
         continue;
       if (FD_ISSET(c->fd, &read_sockets))
-        handle_client(c, server_buf, response_buf, read_sockets);
+        handle_client(c, server_buf, response_buf, ws_handshake_error_buf,
+                      read_sockets, websocket_clients, clients);
     }
   }
   return 0;
