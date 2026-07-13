26e628eb2447485e9e54f3ae73da3bbe66c20559
fauzan <fauzantahir660@gmail.com>
2026-07-13 16:43:31 +0500

    commented previous unused code

--- implementation/ws.cpp
+++ implementation/ws.cpp
@@ -71,39 +71,40 @@ int handle_request_ws(Client *c, map<string, string> headers_map,
   return send_status;
 }
 
-void handle_websocket_client(Client *ws_c, Client clients[],
-                             Client websocket_clients[]) {
-
-  static uint8_t ws_buf[WS_BUF_SIZE]; // static = local to this translation unit
-
-  int ws_recv_status = ::recv(ws_c->fd, ws_buf, WS_BUF_SIZE - 1, 0);
-  if (ws_recv_status == 0) {
-    close_socket(ws_c);
-    return;
-  }
-  if (ws_recv_status < 0) {
-    if (errno == EAGAIN || errno == EWOULDBLOCK)
-      return;
-    close_socket(ws_c);
-    return;
-  }
-  parse_websocket_request(ws_buf, ws_recv_status);
-
-  // Action action = get_client_action(json_req_object);
-  // handle_action(action, json_req_object);
-
-  WS_Frame_Client frame_client;
-  char client_str[] =
-      "<h1>Hello from server</h1><p>WebSocket connection works</p>";
-  frame_client.parse_response(client_str);
-  vector<uint8_t> bytes_response = frame_client.parse_bytes();
-
-  int ws_send_status =
-      ::send(ws_c->fd, bytes_response.data(), bytes_response.size(), 0);
-  if (ws_send_status <= 0) {
-    close_socket(ws_c);
-    return;
-  }
-  printf("successfully sent data to ws %d. \n", ws_c->fd);
-}
+// void handle_websocket_client(Client *ws_c, Client clients[],
+//                              Client websocket_clients[]) {
+//
+//   static uint8_t ws_buf[WS_BUF_SIZE]; // static = local to this translation
+//   unit
+//
+//   int ws_recv_status = ::recv(ws_c->fd, ws_buf, WS_BUF_SIZE - 1, 0);
+//   if (ws_recv_status == 0) {
+//     close_socket(ws_c);
+//     return;
+//   }
+//   if (ws_recv_status < 0) {
+//     if (errno == EAGAIN || errno == EWOULDBLOCK)
+//       return;
+//     close_socket(ws_c);
+//     return;
+//   }
+//   parse_websocket_request(ws_buf, ws_recv_status);
+//
+//   // Action action = get_client_action(json_req_object);
+//   // handle_action(action, json_req_object);
+//
+//   WS_Frame_Client frame_client;
+//   char client_str[] =
+//       "<h1>Hello from server</h1><p>WebSocket connection works</p>";
+//   frame_client.parse_response(client_str);
+//   vector<uint8_t> bytes_response = frame_client.parse_bytes();
+//
+//   int ws_send_status =
+//       ::send(ws_c->fd, bytes_response.data(), bytes_response.size(), 0);
+//   if (ws_send_status <= 0) {
+//     close_socket(ws_c);
+//     return;
+//   }
+//   printf("successfully sent data to ws %d. \n", ws_c->fd);
+// }
 #include <iostream>
